df4cf0f2bedb27a7268330703c8f8beb66686cd7
Name LastName <name56464@domen.ru>
2023-08-14 13:09:58 +0300

    Created data file

---
+++ data.py
@@ -0,0 +1,11 @@
+users = [
+    {"id": 1, "first_name": "Hudson", "last_name": "Pauloh", "age": 31, "email": "huds@mymail.com", "role": "customer",
+     "phone": "661"},
+    {"id": 2, "first_name": "Watson", "last_name": "John", "age": 43, "email": "whats@mymail.com", "role": "executor",
+     "phone": "551"}]
+
+orders = [{"id": 0, "name": "Встреча", "description": "Встретить на вокзале",
+           "start_date": '08/10/2023', "end_date": '08/11/2023', "address": "ul.Sovetov 22",
+           "price": 300, "customer_id": 1, "executor_id": 2}]
+
+offers = [{"id": 0, "order_id": 0, "executor_id": 2}]
